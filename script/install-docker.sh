6c9c5078888e6d53b677d93b7a08a42fb1dad9eb
Lory Hou <lory.hou@augmentum.com>
2017-06-19 11:07:47 +0800

    add docker install-docker.sh

---
+++ script/install-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh
+echo "DOCKER_OPTS=\"\$DOCKER_OPTS --registry-mirror=https://sc9jx4el.mirror.aliyuncs.com\"" | sudo tee -a /etc/default/docker
+sudo service docker restart
+# curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+# to support download compose offline, it maybe slow to download from github
+sudo cp /vagrant/docker-compose-install/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -a -G docker vagrant
