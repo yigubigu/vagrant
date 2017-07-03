4c1723d441cad3abaeaea0705650a60c8bc37cd5
Lory Hou <lory.hou@augmentum.com>
2017-07-03 21:51:38 +0800

    fix path

--- script/install-java.sh
+++ script/install-java.sh
@@ -5,5 +5,5 @@ sudo ln -s /usr/local/java/jdk1.8.0_111/bin/java /usr/bin/java
 echo 'export JAVA_HOME=/usr/local/java/jdk1.8.0_111/' >> ~/.profile
 source ~/.profile
 echo $JAVA_HOME
-sudo cp java-security/local_policy.jar /usr/local/java/jdk1.8.0_111/jre/lib/security/local_policy.jar
-sudo cp java-security/US_export_policy.jar /usr/local/java/jdk1.8.0_111/jre/lib/security/US_export_policy.jar
+sudo cp /vagrant/java-security/local_policy.jar /usr/local/java/jdk1.8.0_111/jre/lib/security/local_policy.jar
+sudo cp /vagrant/java-security/US_export_policy.jar /usr/local/java/jdk1.8.0_111/jre/lib/security/US_export_policy.jar
